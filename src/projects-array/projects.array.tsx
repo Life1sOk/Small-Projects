85dd1ef004c3d0415250c06488e5e54c00ea94d1
Life1sOk <mr.lifeisok@gmail.com>
2022-06-29 21:45:14 +0600

    project 3 will be from scratch

--- src/projects-array/projects.array.tsx
+++ src/projects-array/projects.array.tsx
@@ -1,6 +1,5 @@
 import BG from "../projects-code/background-generator/background-generator";
 import Robolist from "../projects-code/robolist/robolist.component";
-import ToDo from "../projects-code/ToDo/ToDo.component";
 
 export const projects = [
     { 
@@ -14,11 +13,5 @@ export const projects = [
       name: 'Robolist',
       src: require('../img/robolist.gif'),
       project: <Robolist />,
-    },
-    { 
-      id: 2,
-      name: 'To-Do',
-      src: require('../img/robolist.gif'),
-      project: <ToDo />,
     },
 ]
